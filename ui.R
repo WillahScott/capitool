f7f48246424361d13f69b9d05d9aca85db4916ae
Willie Monge <willie9monge@gmail.com>
2015-03-21 11:29:48 -0400

    first commit

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+# Define UI for CapiTool App
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("CapiTool"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
